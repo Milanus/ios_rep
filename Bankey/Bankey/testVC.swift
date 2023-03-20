fc00dd6825ab6f19e7714802ea12c4540cb202ab
porenut1 <porenut@gmail.com>
2023-03-20 10:46:19 +0100

    add onboarding controller

---
+++ Bankey/Bankey/testVC.swift
@@ -0,0 +1,14 @@
+//
+//  testVC.swift
+//  Bankey
+//
+//  Created by Milan Schon on 03/01/2023.
+//
+
+import UIKit
+
+class testVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
